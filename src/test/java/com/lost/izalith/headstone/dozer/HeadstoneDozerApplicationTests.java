c497289736113b8c02a41e3ac08e4cc777198901
mkryvitski <mkryvitski@exadel.com>
2018-07-13 17:35:15 +0300

    feature: initial set up from SPRING INITIALIZR

---
+++ src/test/java/com/lost/izalith/headstone/dozer/HeadstoneDozerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lost.izalith.headstone.dozer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeadstoneDozerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
