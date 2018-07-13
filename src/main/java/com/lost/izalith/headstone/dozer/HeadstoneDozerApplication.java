1b18e44bf4b81cde4a258c1b40e6c84c7fe09013
solairerove <hax2033@gmail.com>
2018-07-13 17:38:23 +0300

    feature: initial set up from SPRING INITIALIZR

---
+++ src/main/java/com/lost/izalith/headstone/dozer/HeadstoneDozerApplication.java
@@ -0,0 +1,12 @@
+package com.lost.izalith.headstone.dozer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadstoneDozerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadstoneDozerApplication.class, args);
+	}
+}
